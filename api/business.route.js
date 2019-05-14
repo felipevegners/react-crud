c273bc3461cf6da8cccc02085592d87e12d2d949
FelipeVegners <felipevegners@gmail.com>
2019-05-14 11:04:04 -0300

    edit component update

--- api/business.route.js
+++ api/business.route.js
@@ -26,7 +26,7 @@ businessRoutes.route('/').get(function (req, res) {
 })
 
 //  Defined edit route
-businessRoutes.route('./edit/:id').get(function (req, res) {
+businessRoutes.route('/edit/:id').get(function (req, res) {
   let id = req.params.id
   Business.findById(id, function (err, business) {
     res.json(business)
