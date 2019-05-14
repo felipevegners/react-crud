35fa89966db1530a51c7b14ed603d553656e675c
Felipe Vegners <felipevegners@gmail.com>
2019-05-13 23:44:29 -0300

    Axios added

--- api/business.route.js
+++ api/business.route.js
@@ -16,7 +16,7 @@ businessRoutes.route('/add').post(function (req, res) {
 
 //  Defined list route
 businessRoutes.route('/').get(function (req, res) {
-  Business.find(function(err, business) {
+  Business.find(function (err, business) {
     if (err) {
       console.log(err)
     } else {
@@ -28,37 +28,37 @@ businessRoutes.route('/').get(function (req, res) {
 //  Defined edit route
 businessRoutes.route('./edit/:id').get(function (req, res) {
   let id = req.params.id
-  Business.findById(id, function(err, business) {
+  Business.findById(id, function (err, business) {
     res.json(business)
   })
 })
 
 //  Defined update route
 businessRoutes.route('/update/:id').post(function (req, res) {
-    Business.findById(req.params.id, function(err, business) {
+  Business.findById(req.params.id, function (err, business) {
     if (!business)
       res.status(404).send("data is not found");
     else {
-        business.person_name = req.body.person_name;
-        business.business_name = req.body.business_name;
-        business.business_gst_number = req.body.business_gst_number;
+      business.person_name = req.body.person_name;
+      business.business_name = req.body.business_name;
+      business.business_gst_number = req.body.business_gst_number;
 
-        business.save().then(business => {
-          res.json('Update complete');
+      business.save().then(business => {
+        res.json('Update complete');
       })
-      .catch(err => {
-            res.status(400).send("unable to update the database");
-      });
+        .catch(err => {
+          res.status(400).send("unable to update the database");
+        });
     }
   });
 });
 
 // Defined delete | remove | destroy route
 businessRoutes.route('/delete/:id').get(function (req, res) {
-    Business.findByIdAndRemove({_id: req.params.id}, function(err, business){
-        if(err) res.json(err);
-        else res.json('Successfully removed');
-    });
+  Business.findByIdAndRemove({ _id: req.params.id }, function (err, business) {
+    if (err) res.json(err);
+    else res.json('Successfully removed');
+  });
 });
 
 module.exports = businessRoutes
--- api/server.js
+++ api/server.js
@@ -8,12 +8,12 @@ const config = require('./DB.js')
 const businessRoute = require('./business.route')
 
 mongoose.Promise = global.Promise
-mongoose.connect(config.DB, { useNewUrlParser: true })
-  .then(() => { console.log('Database is connected bro!') },
+mongoose.connect(config.DB, { useNewUrlParser: true }).then(
+  () => { console.log('Database is connected bro!') },
   err => { console.log('Can not connect to the database' + err) }
   )
 
-app.use(cors)
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
