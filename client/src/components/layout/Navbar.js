25bd66e6fda8b6b351df203a2d673fef085a6f62
FelipeVegners <felipevegners@gmail.com>
2019-08-06 10:46:01 -0300

    setup router and redux structure

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import { Link, BrowserRouter as Router } from 'react-router-dom'
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div className="navbar-fixed">
+        <nav className="z-depth-0">
+          <div className="nav-wrapper white">
+            <Router>
+              <Link
+                to="/"
+                className="col s5 brand-logo center black-text"
+              >
+              <i className="material-icons">code</i>
+              MERN
+              </Link>              
+            </Router>
+          </div>
+        </nav>
+      </div>
+    )
+  }
+}
+
+export default Navbar
