687297be5f88677c6caf71ee3aa7609a841aef19
FelipeVegners <felipevegners@gmail.com>
2019-08-12 11:36:50 -0300

    change user registration and login route

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import Navbar from "./components/layout/Navbar"
 import Landing from "./components/layout/Landing"
 import Register from "./components/auth/Register"
 import Login from "./components/auth/Login"
+import Business from "./components/business/List"
 import PrivateRoute from "./components/private-route/PrivateRoute"
 import Dashboard from "./components/dashboard/Dashboard"
 // Check for token to keep user logged in
@@ -39,6 +40,7 @@ class App extends Component {
             <Route exact path="/" component={Landing} />
             <Route exact path="/register" component={Register} />
             <Route exact path="/login" component={Login} />
+            <Route exact path="/business" component={Business} />
             <Switch>
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
             </Switch>
